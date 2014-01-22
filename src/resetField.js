c3ffa258ce5c990447539442ad9ecb7e7dcceae6
Ahmad Mageed <amageed@gmail.com>
2014-01-21 23:31:11 -0600

    Add curly braces

--- src/resetField.js
+++ src/resetField.js
@@ -5,10 +5,12 @@ angular.module('am.resetField', []).directive('amResetField', ['$compile', '$tim
     link: function(scope, el, attrs, ctrl) {
       // limit to input element of specific types
       var inputTypes = /text|search|tel|url|email|password/i;
-      if (el[0].nodeName !== "INPUT")
+      if (el[0].nodeName !== "INPUT") {
         throw new Error("resetField is limited to input elements");
-      if (!inputTypes.test(attrs.type))
+      }
+      if (!inputTypes.test(attrs.type)) {
         throw new Error("Invalid input type for resetField: " + attrs.type);
+      }
 
       // compiled reset icon template
       var template = $compile('<i ng-show="enabled" ng-mousedown="reset()" class="fa fa-times-circle"></i>')(scope);
